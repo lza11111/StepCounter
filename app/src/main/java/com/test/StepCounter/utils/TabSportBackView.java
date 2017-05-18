a8b5e55299d4b2a413c25f8575a5bc36bc07df8e
limbochao <m15968802019@163.com>
2017-05-18 23:14:26 +0800

    界面
    增加跑步界面

---
+++ app/src/main/java/com/test/StepCounter/utils/TabSportBackView.java
@@ -0,0 +1,48 @@
+package com.test.StepCounter.utils;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.test.StepCounter.R;
+
+/**
+ * 运动界面的背景View
+ * Created by liuheng on 16/7/16.
+ */
+public class TabSportBackView extends View {
+
+    private Paint mPaint;
+    private int mBackHeight,mBackBottomHeight;
+    private Path mBottomPath;
+
+    public TabSportBackView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        int backColor = context.getResources().getColor(R.color.sport_bg);
+        mPaint = new Paint();
+        mPaint.setColor(backColor);
+        mPaint.setStyle(Paint.Style.FILL);
+
+        mBottomPath =new Path();
+
+        mBackHeight = getResources().getDimensionPixelSize(R.dimen.tab_sport_top_height);
+        mBackBottomHeight = getResources().getDimensionPixelSize(R.dimen.tab_sport_oval_half_height);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawRect(0, 0, getMeasuredWidth(), mBackHeight, mPaint);
+
+        mBottomPath.moveTo(0, mBackHeight);
+        mBottomPath.lineTo(getMeasuredWidth()/2, mBackHeight + mBackBottomHeight);
+        mBottomPath.lineTo(getMeasuredWidth(), mBackHeight);
+        mBottomPath.close();
+        canvas.drawPath(mBottomPath,mPaint);
+    }
+}
